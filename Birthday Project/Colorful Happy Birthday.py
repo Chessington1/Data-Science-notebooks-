0389494d2c78375159b34dbd5d9a44b775f74364
Emmanuel Nyamekye <nyamekyee191@gmail.com>
2025-10-22 02:01:51 -0800

    modified a python file

--- Birthday Project/Colorful Happy Birthday.py
+++ Birthday Project/Colorful Happy Birthday.py
@@ -1,55 +1,44 @@
-# 🎉 Colorful Happy Birthday with Real Music
-from playsound import playsound
-import threading
 import time
+import threading
 from random import randint
+from playsound import playsound
+
 def play_music():
-    playsound('happy_birthday.mp3')
-    thread = threading.Thread(target=play_music)
-    thread.start()
-    
-name = "Emmanuel"
+    playsound('happy_birthday.mp3')  # Your renamed music file
 
-colors = [
-    "\033[91m", "\033[92m", "\033[93m",
-    "\033[94m", "\033[95m", "\033[96m", "\033[97m"
-]
-reset = "\033[0m"
+def animation():
+    for i in range(1, 85):
+        print('')  # Adds blank lines for spacing
 
-# Play background music in a separate thread
-def play_music():
-    playsound("happy_birthday.mp3")  # Put your song file in the same folder
-
-thread = threading.Thread(target=play_music)
-thread.start()
-
-for i in range(1, 85):
-    print('')
-
-space = ''
-for i in range(1, 1000):
-    count = randint(1, 100)
-    while count > 0:
-        space += ' '
-        count -= 1
-
-    color = colors[randint(0, len(colors) - 1)]
-
-    if i % 10 == 0:
-        print(space + color + f'🎂 Happy Birthday, {name}! 🎉' + reset)
-    elif i % 9 == 0:
-        print(space + color + '🎂' + reset)
-    elif i % 5 == 0:
-        print(space + color + '💛' + reset)
-    elif i % 8 == 0:
-        print(space + color + '🍕' + reset)
-    elif i % 7 == 0:
-        print(space + color + '🍫' + reset)
-    elif i % 6 == 0:
-        print(space + color + f'Happy Birthday, {name}! 💖' + reset)
-    else:
-        print(space + color + '💎' + reset)
-
-    time.sleep(0.25)
     space = ''
+    for i in range(1, 1000):
+        count = randint(1, 100)
+        space = ' ' * count
+
+        if i % 10 == 0:
+            print(space + '🎂 Happy Birthday!')
+        elif i % 9 == 0:
+            print(space + '🎉')
+        elif i % 5 == 0:
+            print(space + '💛')
+        elif i % 8 == 0:
+            print(space + '🍕')
+        elif i % 7 == 0:
+            print(space + '🍫')
+        elif i % 6 == 0:
+            print(space + 'Happy Birthday! 💖')
+        else:
+            print(space + '🔶')
+
+        time.sleep(0.2)
+
+while True:
+    # Start the song and animation together
+    music_thread = threading.Thread(target=play_music)
+    music_thread.start()
+
+    animation()
+
+    print("\n🎶 Replaying... 🎶\n")
+    time.sleep(2)  # Short pause before looping
 
